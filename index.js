4d5261adc3a56b9d999fc9c095a0fff6a4f98a6d
Felipe Reinaux <felipereinaux@gmail.com>
2020-08-27 11:43:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ const wishList = [
 ]
 
 let total = 0
-let arrecadado = 435
+let arrecadado = 485
 let arrecadadoItens = arrecadado
 
 $(wishList.sort((a,b) => a.value - b.value)).each((index, item) => {
